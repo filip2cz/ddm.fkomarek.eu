580b8120ace7c55720866334ec1dedbb893263f6
filip2cz <filip@fkomarek.eu>
2023-10-16 12:58:05 +0200

    DEL: try, kvůli jednoduchosti

--- programovani/2001/kalkulacka3.cs
+++ programovani/2001/kalkulacka3.cs
@@ -1,12 +1,5 @@
 Console.Write("Zadejte matematický příklad: ");
 string priklad = Console.ReadLine();
 
-try
-{
-    double vysledek = Convert.ToDouble(new System.Data.DataTable().Compute(priklad, null));
-    Console.WriteLine("Výsledek: " + vysledek);
-}
-catch (Exception ex)
-{
-    Console.WriteLine("Chyba: " + ex.Message);
-}
+double vysledek = Convert.ToDouble(new System.Data.DataTable().Compute(priklad, null));
+Console.WriteLine("Výsledek: " + vysledek);

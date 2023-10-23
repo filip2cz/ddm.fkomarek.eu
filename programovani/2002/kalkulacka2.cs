1c9fba27265d6a4344f51a9955f24f685990c773
Filip Komárek <filip2cz>
2023-10-23 21:30:51 +0200

    EDIT: přesun podmínek do samostatné hodiny

---
+++ programovani/2002/kalkulacka2.cs
@@ -0,0 +1,30 @@
+Console.Write("Zadejte první číslo: ");
+int cislo1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Zadejte druhé číslo: ");
+int cislo2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Zadejte operaci (+, -, *, /): ");
+string operace = Console.ReadLine();
+
+if (operace == "+")
+{
+    Console.WriteLine("Součet čísel " + cislo1 + " a " + cislo2 + " je " + (cislo1 + cislo2) + ".");
+}
+if (operace == "-")
+{
+    Console.WriteLine("Rozdíl čísel " + cislo1 + " a " + cislo2 + " je " + (cislo1 - cislo2) + ".");
+}
+if (operace == "*")
+{
+    Console.WriteLine("Součin čísel " + cislo1 + " a " + cislo2 + " je " + (cislo1 * cislo2) + ".");
+}
+if (operace == "/")
+{
+    if (cislo2 == 0)
+    {
+        Console.WriteLine("Nulou nelze dělit");
+    }
+    else
+    {
+        Console.WriteLine("Podíl čísel " + cislo1 + " a " + cislo2 + " je " + (cislo1 / cislo2) + ".");
+    }
+}

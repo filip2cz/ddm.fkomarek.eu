df1c72a86fac15afb78c57baec9bb2cf13eac669
Filip Komárek <filip2cz>
2023-10-29 21:15:05 +0100

    EDIT: Mikyho návrhy na zlepšení

--- programovani2/1001/networkinfoverkratke.cs
+++ programovani2/1001/networkinfoverkratke.cs
@@ -1,3 +1,3 @@
-WebClient client = new WebClient();
-int cislo = int.Parse(await client.DownloadStringTaskAsync("https://raw.githubusercontent.com/filip2cz/network-info/main/ver"));
+HttpClient client = new HttpClient();
+int cislo = int.Parse(client.GetStringAsync("https://raw.githubusercontent.com/filip2cz/network-info/main/ver").Result);
 Console.WriteLine(cislo - 1);

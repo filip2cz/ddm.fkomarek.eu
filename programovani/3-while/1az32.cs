64fe6b630e70add2525d3707e6d6b4c5652789fc
Filip Komárek <filip2cz>
2023-12-03 13:51:29 +0100

    ADD: PROG1 - while

---
+++ programovani/3-while/1az32.cs
@@ -0,0 +1,16 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        int i = 1;
+
+        while (i &lt;= 32)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+
+        Console.ReadLine();
+    }
+}

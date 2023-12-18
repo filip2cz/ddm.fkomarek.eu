dcdd2dd948ba8fcd14bf934d076bcfb3055cb36d
Filip Komárek <filip2cz>
2023-12-18 15:53:56 +0100

    ADD: PROG1 - cvičení: úprava kódu

---
+++ programovani/4-upravakodu/3.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        if (5 == 7)
+        {
+            Console.WriteLine("ANO");
+        }
+
+        Console.ReadLine();
+    }
+}

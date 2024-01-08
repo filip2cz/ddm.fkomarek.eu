db4fb13b8274f3dbb51f6d52f01a4abef96a73c3
Filip Komárek <filip2cz>
2024-01-08 15:30:56 +0100

    ADD: síťování pro PROG2

--- programovani2/1-sitovani/five.cs
+++ programovani2/1-sitovani/five.cs
@@ -8,7 +8,7 @@ class Program
     {
         // Nastavení IP adresy a portu serveru
         string serverIp = "ssfs.fkomarek.eu";
-        int serverPort = 4445;
+        int serverPort = 4447;
 
         // Vytvoření TcpClient pro připojení k serveru
         using (TcpClient client = new TcpClient(serverIp, serverPort))
@@ -16,6 +16,16 @@ class Program
             // Otevření síťového streamu pro komunikaci
             using (NetworkStream stream = client.GetStream())
             {
+
+                // Získání od uživatele string, co budeme odesílat
+                Console.Write("Zadejte text pro odeslání: ");
+                string text = Console.ReadLine();
+
+                // Převod textu na pole bytů, abychom ho mohli poslat přes síť
+                byte[] dataSend = Encoding.ASCII.GetBytes(text);
+
+                stream.Write(dataSend, 0, dataSend.Length);
+
                 // Vytvoření byte array pro přijímání dat
                 byte[] buffer = new byte[1024];
 

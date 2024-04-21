84f22107a7a6ef5bb2b09dc8b48bdacdc332f79c
filip2cz <filip@fkomarek.eu>
2024-04-21 14:47:48 +0200

    EDIT: repair command

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd ./programovani/0-template/ && php index.php > index.html && ../../
+cd ./programovani/0-template/ && php index.php > index.html && cd ../../
 
 # https://answers.netlify.com/t/directory-listing/14246/6

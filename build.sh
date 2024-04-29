8a69aa1580236e3f6b6f2374983f575ba3477864
filip2cz <filip@fkomarek.eu>
2024-04-29 13:20:49 +0200

    ADD: build 3-semafor

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 cd ./programovani/4-rgb-led && php index.php > index.html && rm index.php && cd ../../
 cd ./programovani/4-hledanichyb && php index.php > index.html && rm index.php && cd ../../
 
+cd ./robotika/3-semafor && php index.php > index.html && rm index.php && cd ../../
+
 # https://answers.netlify.com/t/directory-listing/14246/6

efb165990162adef44c8e93840ad936b9774fa02
filip2cz <filip@fkomarek.eu>
2024-04-29 13:16:20 +0200

    ADD: semafor

---
+++ robotika/3-semafor/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Semafor</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        -->
+
+        <p>
+            Vytvořte semafor pomocí otočných potorů, na kterých budou barvy zelená a červená.
+            Pokud senzor vzdálenosti uvidí auto ve vedlejší ulici, dá hlavní silnici červenou a vedlenší ulici zelenou.
+            Silnice a semafory budou vypadat podle následujících nákresů:
+        </p>
+
+        <img src="./semafor1.png" alt="Obrázek: nákres semaforu podle popisu výše">
+
+        <p>
+            Pokud již máte vaší práci hotovou, přidejte na semafor žlutou barvu,
+            která se na vteřinu objeví pro všechny silnice a tím upozorní řidiče na změnu barvy.
+        </p>
+
+        <img src="./semafor2.png" alt="Obrázek: nákres semaforu podle popisu výše, neboli se žlutou barvou">
+
+        <p>
+            Dále k semaforu můžete přidat například závoru, která zavře vedlejší ulici, pokud má červenou.
+            Počítáme s tím, že vedlejší ulice je jednosměrná, takže auta z hlavní silnice do ní odbočovat nesmí.
+        </p>
+
+        <img src="./semafor3.png" alt="Obrázek: nákres semaforu podle popisu výše, neboli se žlutou barvou a se závorou">
+
+    </div>
+
+</body>
+
+</html>

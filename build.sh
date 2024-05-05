90c5ebe6f892a0b26f5dcc6c36bbacd053efb2b3
filip2cz <filip@fkomarek.eu>
2024-05-05 16:50:14 +0200

    ADD: 4-fotorezistor

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd ./programovani/4-rgb-led && php index.php > index.html && rm index.php && cd ../../
 cd ./programovani/4-hledanichyb && php index.php > index.html && rm index.php && cd ../../
+cd ./programovani/4-fotorezistor && php index.php > index.html && rm index.php && cd ../../
 
 cd ./robotika/3-semafor && php index.php > index.html && rm index.php && cd ../../
 

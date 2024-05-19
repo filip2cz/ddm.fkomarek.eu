cfba6ed7d3f9cbaf06f99cafbb82c50dcea1ee12
filip2cz <filip@fkomarek.eu>
2024-05-19 16:56:20 +0200

    ADD: nějaké úlohy pro procvičování

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ cd ./programovani/4-rgb-led && php index.php > index.html && rm index.php && cd 
 cd ./programovani/4-hledanichyb && php index.php > index.html && rm index.php && cd ../../
 cd ./programovani/4-fotorezistor && php index.php > index.html && rm index.php && cd ../../
 cd ./programovani/4-display && php index.php > index.html && rm index.php && cd ../../
+cd ./programovani/4-procvicovani && php index.php > index.html && rm index.php && cd ../../
 
 cd ./robotika/3-semafor && php index.php > index.html && rm index.php && cd ../../
 
---
+++ programovani/4-procvicovani/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <p class="test-link">ERROR: načtení souboru link.html bylo neúspěšné</p>
+
+        <h1 class="nadpis">Úlohy na procvičení</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        <pre><code class="language-csharp">C# kód</code></pre>
+
+        <pre><code class="language-arduino">Arduino kód</code></pre>
+
+        <p class="output">Výstup z konzole</p>
+
+        <button class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#reseni1">řešení</button>
+        <div id="reseni1" class="collapse mt-3">
+            <pre>řešení</pre>
+        </div>
+
+        <button class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#popis1">Podrobný popis kódu</button>
+        <div id="popis1" class="collapse mt-3">
+            <ul>
+                <li>
+                    Nějaký popis
+                </li>
+
+                <br>
+                <h5>První část:</h5>
+
+                <li>
+                    Popis první části
+                </li>
+
+                <br>
+                <h5>Druhá část:</h5>
+
+                <li>
+                    Popis druhé části
+                </li>
+            </ul>
+        </div>
+
+        -->
+
+        <p>
+            Tuto lekci zasvětíme procvičování našich programovacích znalostí z předchozích hodin pomocí několika úloh. Každou z těchto úloh mi poté odevzdejte na Google Classroom.
+        </p>
+
+        <p>
+            Tuto lekci budeme pracovat se simulátorem dostupným zde: <a href="https://wokwi.com/projects/397046750664768513" target="_blank">https://wokwi.com/projects/397046750664768513</a>
+        </p>
+
+        <h2>Geometrická kalkulačka</h2>
+
+        <p>
+            Vytvořte kalkulačku, která bude umět vypočítat obvod a obsah několika geometrických tvarů. Uživatel si vybere, který tvar chce použít (čtverec, obdélník, kruh, ...) a poté zadá požadované hodnoty. Program mu poté vypíše obvod a obsah. Nebojte se použít internet pro nalezení vzorců pro výpočet, pokud si je nepamatujete.<br>
+            Dále také udělejte to, že pokud bude výpočet úspěšný, rozsvítíte zelenou led diodu. Pokud bude výpočet neúspěšný (uživatel zadá špatné hodnoty, například zápornou hodnotu u délky strany), rozsvítí se červená led dioda.
+        </p>
+
+        <p><b>
+            Nezapomeňte odevzdat vaše řešení na Google učebnu: <a href="" target="_blank"></a>
+        </b></p>
+
+        <h3>Poznámka: další úlohy doplním během několika hodin, nepozději do dnešního večera.</h3>
+
+    </div>
+
+</body>
+
+</html>

36f8d5da1beaa802699f362ae60eae6b6fa90d9b
filip2cz <filip@fkomarek.eu>
2024-05-27 13:55:31 +0200

    ADD: elektrická kytara zadání

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ cd ./programovani/4-display && php index.php > index.html && rm index.php && cd 
 cd ./programovani/4-procvicovani && php index.php > index.html && rm index.php && cd ../../
 
 cd ./robotika/3-semafor && php index.php > index.html && rm index.php && cd ../../
+cd ./robotika/3-kytara && php index.php > index.html && rm index.php && cd ../../
 
 # https://answers.netlify.com/t/directory-listing/14246/6
---
+++ robotika/3-kytara/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include ("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Elektrická kytara</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        -->
+
+        <p>
+            V dnešní hodině si vytvoříme elektrickou kytaru. Tuto kytaru budeme ovládat pomocí vzdálenostního senzoru.
+            Bude to fungovat tak, že se bude výška tónu určovat podle toho, v jaké vzdálenosti nás vzdálenostní senzor
+            vidí naši ruku.
+        </p>
+
+    </div>
+
+</body>
+
+</html>

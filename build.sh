955546ea74e079acf92cd51fc826980c115656f0
filip2cz <filip@fkomarek.eu>
2024-08-31 20:49:57 +0200

    EDIT: better build script

--- build.sh
+++ build.sh
@@ -1,13 +1,30 @@
-#!/bin/sh
-cd ./programovani/4-rgb-led && php index.php > index.html && rm index.php && cd ../../
-cd ./programovani/4-hledanichyb && php index.php > index.html && rm index.php && cd ../../
-cd ./programovani/4-fotorezistor && php index.php > index.html && rm index.php && cd ../../
-cd ./programovani/4-display && php index.php > index.html && rm index.php && cd ../../
-cd ./programovani/4-procvicovani && php index.php > index.html && rm index.php && cd ../../
-cd ./programovani/3-formsy && php index.php > index.html && rm index.php && cd ../../
+#!/bin/bash
 
-cd ./robotika/0-pravidla && php index.php > index.html && rm index.php && cd ../../
-cd ./robotika/3-semafor && php index.php > index.html && rm index.php && cd ../../
-cd ./robotika/3-kytara && php index.php > index.html && rm index.php && cd ../../
+# Funkce pro zpracování PHP souboru a jeho konverzi na HTML
+process_php_to_html() {
+    php_file="$1"
+    html_file="${php_file%.php}.html"
+
+    # Spustí PHP soubor a uloží výstup do HTML
+    php "$php_file" > "$html_file"
+
+    # Zkontroluje, jestli byl soubor úspěšně převeden
+    if [ -f "$html_file" ]; then
+        echo "Převedeno: $php_file -> $html_file"
+
+        # Smaže původní PHP soubor
+        rm "$php_file"
+        echo "Smazáno: $php_file"
+    else
+        echo "Chyba při převodu: $php_file"
+    fi
+}
+
+# Prohledá všechny index.php soubory ve složce a podadresářích
+find . -name "index.php" | while read php_file; do
+    process_php_to_html "$php_file"
+done
+
+echo "Všechny PHP soubory byly úspěšně převedeny a původní PHP soubory smazány."
 
 # https://answers.netlify.com/t/directory-listing/14246/6

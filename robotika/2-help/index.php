2dee3319d43a90f633d79f25309fe46ffc9b6eef
filip2cz <filip@fkomarek.eu>
2024-10-06 14:27:26 +0200

    ADD: poznávání barev

---
+++ robotika/2-help/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Název</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        -->
+
+        <p>
+            Dnes si uděláme první lekci ze sbírky úloh. Oficální sbírka úloh je v angličtině, ale pokud někdo neumí
+            dostatečně anglicky, je k dispozici strojový překlad v prohlížečové verzi.
+        </p>
+
+        <img src="./cesta1.png">
+
+        <img src="./cesta2.png">
+
+    </div>
+
+</body>
+
+</html>

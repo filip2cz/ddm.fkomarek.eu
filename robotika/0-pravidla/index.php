264e712d9fff275ddd2e26c1eb1766ecd667a684
filip2cz <filip@fkomarek.eu>
2024-11-05 21:25:54 +0100

    Revert "ADD: pravidla"
    This reverts commit dac1f73b291a0097d747efef2427adf159e25d4f.

--- robotika/0-pravidla/index.php
+++ robotika/0-pravidla/index.php
@@ -36,8 +36,6 @@
                 Na konci hodiny vracíme počítače a LEGO na určená místa (typicky místo, odkud jste si je mohli vzít).
             </li>
 
-            <br>
-
             <h3>
                 Při jednorázovém porušení pravidel může být provedeno opatření proti porušování těchto pravidel (např.
                 zabavení počítače). Při opakovaném porušování pravidel, nebo při porušení závažného pravidla, může být

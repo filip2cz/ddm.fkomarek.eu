cea5561140ffd0a8c606eb5a069ef182f82d02c2
filip2cz <filip@fkomarek.eu>
2024-11-10 17:03:40 +0100

    ADD: kvíz

---
+++ programovani/3-kviz/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include ("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Kvíz</h1>
+
+        <p>
+            Vytvořte kvíz na libovolné téma o alespoň 4 otázkách. Měla by v něm být alespoň jedna otázka ABC a alespoň
+            jedna otázka s volnou odpovědí. Program dá také uživateli známku (jako ve škole) na základě toho, jak si v
+            testu vedl.
+        </p>
+
+        <p>
+            Kód nezapomeňte uložit na google učebnu.
+        </p>
+
+    </div>
+
+</body>
+
+</html>

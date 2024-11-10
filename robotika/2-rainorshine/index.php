c67edc905ebfc422a699bea1db82404beeee01c7
filip2cz <filip@fkomarek.eu>
2024-11-10 17:29:49 +0100

    ADD: počasí

--- robotika/2-rainorshine/index.php
+++ robotika/2-rainorshine/index.php
@@ -8,16 +8,7 @@
 </head>
 
 <body>
-    <!-- CSS a JavaScript -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-        crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="../../main.css">
-
-    <!-- Support pro mobilní menu -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include ("../../src/link.html"); ?>
 
     <div class="container">
 
@@ -37,6 +28,8 @@
         <img src="./cesta1.png">
 
         <img src="./cesta2.png">
+
+        <a href="https://spike.legoeducation.com/prime/unit-plans/blt36711eeb7dd3d82e/lesson-detail/blt877077dd294ea3da" target="_blank"><button class="btn btn-primary" type="button">Otevřít zadání v prohlížeči</button></a>
 
     </div>
 

074b8c99555b8921661c092cf5cfd8ff4c066eb9
filip2cz <filip@fkomarek.eu>
2024-11-24 21:10:19 +0100

    ADD: váha na planetách

---
+++ programovani/3-vaha/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Váha na planetách</h1>
+
+        <p>
+            Vytvořte program, do kterého uživatel napíše svoji váhu a vybere planetu. Program mu poté vypíše jeho váhu
+            na jím zvolené planetě. Údaje ohledně výpočtu váhy můžete zjistit například zde:<br>
+            <a href="https://www.exploratorium.edu/explore/solar-system/weight"
+                target="_blank">https://www.exploratorium.edu/explore/solar-system/weight</a><br>
+            <a href="https://www.astrovm.cz/cz/pro-navstevniky/aktuality-ak/kolik-bychom-vazili-na-jinych-planetach.html"
+                target="_blank">https://www.astrovm.cz/cz/pro-navstevniky/aktuality-ak/kolik-bychom-vazili-na-jinych-planetach.html</a>
+        </p>
+
+        <h2>
+            Kód nezapomeňte uložit na Google Učebnu: <a href="https://classroom.google.com/c/NzE5NDk1NjM2NDAw/a/NzM1MzA2ODU0NzU5/details" target="_blank">https://classroom.google.com/c/NzE5NDk1NjM2NDAw/a/NzM1MzA2ODU0NzU5/details</a>
+        </h2>
+
+    </div>
+
+</body>
+
+</html>

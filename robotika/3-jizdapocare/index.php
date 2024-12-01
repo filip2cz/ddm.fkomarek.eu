ca3cf1588ff46ba29ac55cb7bce836846ef583fb
filip2cz <filip@fkomarek.eu>
2024-12-01 17:50:14 +0100

    ADD: 3-jizdapocare

---
+++ robotika/3-jizdapocare/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include ("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Jízda po čáře</h1>
+
+        <p>
+            Vytvořte robota, který bude jezdit podél černé čáry a bude schopen dojet do cíle.
+        </p>
+
+    </div>
+
+</body>
+
+</html>

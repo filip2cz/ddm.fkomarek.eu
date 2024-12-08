47dc8acd2c2645e111a08f5991195198ee1072db
filip2cz <filip@fkomarek.eu>
2024-12-08 15:36:50 +0100

    ADD: automatická závora

---
+++ robotika/3-zavora/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Automatická závora</h1>
+
+        <p>
+            Vytvořte závoru, která se autometicky otevírá a zavírá. Závora se otevře pouze pro zelená auta. Závora se
+            zavře, pokud již auto není v prostoru závory. Pokud se auto zastaví, závora čeká.
+        </p>
+
+    </div>
+
+</body>
+
+</html>

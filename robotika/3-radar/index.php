1e7792485880a28439db795e76f7490f9f534dd5
filip2cz <filip@fkomarek.eu>
2025-02-02 15:43:35 +0100

    ADD: radar

---
+++ robotika/3-radar/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include ("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Radar</h1>
+
+        <p>
+            Postavte radar, který bude senzor na vzdálenost namířený šikmo nahoru, jenž se bude otáčet. Pokud senzor
+            najde nějaký objekt, zastaví se a začne vydávat výstražný zvuk.
+        </p>
+
+    </div>
+
+</body>
+
+</html>

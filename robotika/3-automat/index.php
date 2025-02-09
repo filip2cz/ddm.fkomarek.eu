2193dbc3ea75f1c4f107d287fd83f1d9bce7378d
filip2cz <filip@fkomarek.eu>
2025-02-09 21:02:26 +0100

    ADD: herní automat

---
+++ robotika/3-automat/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include ("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Herní automat</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        -->
+
+        <p>
+            Vytvořte cokoliv, co se dá brát jako herní automat.
+        </p>
+
+    </div>
+
+</body>
+
+</html>

83f57cf936e48ddbb777d2d4f93812866278e637
Filip Komárek <filip@fkomarek.eu>
2025-02-24 15:42:58 +0100

    ADD: robotika: koncept zadání: tank

---
+++ robotika/3-tank/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include ("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Tank</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        -->
+
+        <p>
+            Postavte model tanku (kvůli limitaci naší lego sady s koly), jenž bude jezdit a pokud před sebou uvidí (pomocí senzoru vzdálenosti) cíl, tak "vystřelí" (např. světlo ze senzoru vzdálenosti, atd.)
+        </p>
+
+
+    </div>
+
+</body>
+
+</html>

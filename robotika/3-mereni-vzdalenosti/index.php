a52f243a1baa2c5653363df46ffdb203f173c8a4
filip2cz <filip@fkomarek.eu>
2025-03-02 15:46:32 +0100

    ADD: měření vzdálenosti černé čáry

---
+++ robotika/3-mereni-vzdalenosti/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include ("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Měření vzdálenosti</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        -->
+
+        <p>
+            V jedné z předchozích hodin jsme si vyrobili robota, který uměl jezdit po černé čáře. Nyní na toto zadání budeme navazovat.
+        </p>
+
+        <p>
+            Vytvořte robota, který dokáže změřit délku této černé čáry v jakýchkoliv jednotkách.
+        </p>
+
+    </div>
+
+</body>
+
+</html>

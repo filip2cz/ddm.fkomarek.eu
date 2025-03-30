da00b6cef0c0546ec7afb11cc352f4b5402a4365
filip2cz <filip@fkomarek.eu>
2025-03-30 15:43:35 +0200

    ADD: chybějící zadání

---
+++ robotika/3-kalkulacka/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include ("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Kalkulačka</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        -->
+
+        <p>
+            Vytvořte na své lego spike kostce program, díky kterému budete moci vybrat dvě čísla a sečíst je.
+        </p>
+
+        <p>
+            Příliš snadné? Přidejte odčítání (stačí i přičítání záporných čísel), násobení a dělení.
+        </p>
+
+    </div>
+
+</body>
+
+</html>
---
+++ robotika/3-smetak/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Robotický smeták</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        -->
+
+        <p>
+            V poli označením černou čarou se nachází další, menší, modré pole a mimo toto modré pole se na náhodných
+            místech nacházejí 3 červené kostičky. Vaším cílem je přemístit je do modrého pole.
+        </p>
+
+    </div>
+
+</body>
+
+</html>

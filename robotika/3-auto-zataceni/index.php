57ccf3a1019aad1fdd21205e4c187d572a677254
filip2cz <filip@fkomarek.eu>
2025-04-06 23:12:16 +0200

    ADD: automobilové zatáčení

---
+++ robotika/3-auto-zataceni/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Automobilové zatáčení</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        -->
+
+        <p>
+            Vytvořte model zatáčení, jenž bude fungoval podobně, jako zatáčení u aut.
+        </p>
+
+        <button class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#imageCollapse101">řešení</button>
+        <div id="imageCollapse101" class="collapse mt-3">
+            <video width="270" height="480" controls>
+                <source src="auto1.mp4" type="video/mp4">
+            </video>
+
+            <video width="270" height="480" controls>
+                <source src="auto2.mp4" type="video/mp4">
+            </video>
+        </div>
+
+    </div>
+
+</body>
+
+</html>

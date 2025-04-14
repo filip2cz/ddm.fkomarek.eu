73af5f413dad09d51247c0dce780c0c38828d9e3
filip2cz <filip@fkomarek.eu>
2025-04-14 15:45:15 +0200

    ADD: ukládání dat

---
+++ robotika/3-ukladani-dat/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <h1 class="nadpis">Ukládání dat</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        -->
+
+        <p>
+            Vytvořte "zápisník", který do kterého bude možné zapisovat informace a bude je po té možné vypsat.
+        </p>
+
+        <h2>Poznámka pro učitele</h2>
+
+        <p>
+            Je dobré na začátku hodiny udělat se studenty "brainstorming" ohledně toho, jak data zapisovat do naší krabičky. Např.:
+            <ul>
+                <li>Výběr znaků (podobně, jako u kalkulačky)</li>
+                <li>znaky jsou očíslované (pro A zmáčknu tlačítko jednou, pro B dvakrát, ...)</li>
+                <li>morseovka</li>
+            </ul>
+        </p>
+
+    </div>
+
+</body>
+
+</html>

6937334363f64fe5c67b384e08c9ea656fb0b04f
filip2cz <filip@fkomarek.eu>
2025-04-14 12:06:45 +0200

    ADD: vyhledávání informací + DateTimePicker

---
+++ programovani/5-vyhledavani_datetimepicker/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <p class="test-link">ERROR: načtení souboru link.html bylo neúspěšné</p>
+
+        <h1 class="nadpis">Vyhledávání informací + DateTimePicker</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        <pre><code class="language-csharp">C# kód</code></pre>
+
+        <pre><code class="language-csharp" id="allowCopy">C# kód</code></pre>
+
+        <pre><code class="language-arduino">Arduino kód</code></pre>
+
+        <p class="output">Výstup z konzole</p>
+
+        <button class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#reseni1">řešení</button>
+        <div id="reseni1" class="collapse mt-3">
+            <pre>řešení</pre>
+        </div>
+
+        <button class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#popis1">Podrobný popis kódu</button>
+        <div id="popis1" class="collapse mt-3">
+            <ul>
+                <li>
+                    Nějaký popis
+                </li>
+
+                <br>
+                <h5>První část:</h5>
+
+                <li>
+                    Popis první části
+                </li>
+
+                <br>
+                <h5>Druhá část:</h5>
+
+                <li>
+                    Popis druhé části
+                </li>
+            </ul>
+        </div>
+
+        -->
+
+        <p>
+            Jedna z důležitých vlastností programátora, na kterou bych se rád v následujících hodinách zaměřil, je
+            schopnost vyhledávání informací po vlastní ose.
+        </p>
+
+        <p>
+            V dnešní hodině se zaměříme na DateTimePicker, jenž umožňuje uživateli zadávat přehledně datum a čas do naší
+            aplikace. Cílem dnešní hodiny je po vlastní ose najít na internetu, jak s tímto prvkem pracovat.
+        </p>
+
+        <h2>
+            Zadání
+        </h2>
+
+        <p>
+            Vytvořte aplikaci, do které uživatel vloží datum a program mu vypíše vzdálenost tohoto data od dnešního dne ve vámi zvolených jednotkách (např. dny, hodiny nebo třeba minuty).
+        </p>
+
+    </div>
+
+</body>
+
+</html>

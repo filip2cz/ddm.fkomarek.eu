e3ebee5cbdbf694c277c71cc78517949c47b0047
filip2cz <filip@fkomarek.eu>
2025-04-27 16:49:39 +0200

    ADD: návod na vytvoření ZIP souboru z projektu pro odevzdání

---
+++ programovani/5-jak-odevzdavat-formsy/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <title>DDM - Filip Komárek</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <meta charset='utf-8'>
+</head>
+
+<body>
+    <?php include("../../src/link.html"); ?>
+
+    <div class="container">
+
+        <a href="../"><button class="btn btn-primary" type="button">Zpět</button></a>
+
+        <p class="test-link">ERROR: načtení souboru link.html bylo neúspěšné</p>
+
+        <h1 class="nadpis">Návod: jak odevzdávat Forms projekty do Google učebny</h1>
+
+        <!--
+
+        <p>
+            Mauris elementum mauris vitae tortor. Nullam rhoncus aliquam metus. Nullam dapibus fermentum ipsum.
+        </p>
+
+        https://www.freeformatter.com/html-escape.html
+
+        <pre><code class="language-csharp">C# kód</code></pre>
+
+        <pre><code class="language-csharp" id="allowCopy">C# kód</code></pre>
+
+        <pre><code class="language-arduino">Arduino kód</code></pre>
+
+        <p class="output">Výstup z konzole</p>
+
+        <button class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#reseni1">řešení</button>
+        <div id="reseni1" class="collapse mt-3">
+            <pre>řešení</pre>
+        </div>
+
+        <button class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#popis1">Podrobný popis kódu</button>
+        <div id="popis1" class="collapse mt-3">
+            <ul>
+                <li>
+                    Nějaký popis
+                </li>
+
+                <br>
+                <h5>První část:</h5>
+
+                <li>
+                    Popis první části
+                </li>
+
+                <br>
+                <h5>Druhá část:</h5>
+
+                <li>
+                    Popis druhé části
+                </li>
+            </ul>
+        </div>
+
+        -->
+        <ol>
+            <h2>
+                <li></li>
+            </h2>
+            <p>
+                <img src="./1.png">
+            </p>
+
+            <h2>
+                <li></li>
+            </h2>
+            <p>
+                <img src="./2.png" style="width: 500px">
+            </p>
+
+            <h2>
+                <li></li>
+            </h2>
+            <p>
+                <img src="./3.png">
+            </p>
+
+            <h2>
+                <li><b>Zavřete Visual Studio</b></li>
+            </h2>
+
+            <h2>
+                <li></li>
+            </h2>
+            <p>
+                <img src="./4.png">
+            </p>
+
+            <h2>
+                <li></li>
+            </h2>
+            <p>
+                <img src="./5.png" style="width: 500px">
+            </p>
+
+            <h2>
+                <li></li>
+            </h2>
+            <p>
+                <img src="./6.png">
+            </p>
+
+            <h2>
+                <li><b>Tento .ZIP soubor, který se Vám vytvořil, nyní můžete nahrát na Google učebnu</b></li>
+            </h2>
+        </ol>
+
+    </div>
+
+</body>
+
+</html>

054cb9809009b6ff6d234196e4f300b05ec4bf1e
filip2cz <filip@fkomarek.eu>
2025-09-15 15:32:40 +0200

    EDIT: přesun z html na php + odkrytí řešení

--- programovani/1-blokoveprogramovani/index.php
+++ programovani/1-blokoveprogramovani/index.php
@@ -6,18 +6,9 @@
     <link rel="icon" type="image/x-icon" href="./favicon.ico">
     <meta charset='utf-8'>
 </head>
-<!-- CSS a JavaScript -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-    crossorigin="anonymous"></script>
-<link rel="stylesheet" type="text/css" href="../../main.css">
 
 <body>
-
-    <!-- Support pro mobilní menu -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include ("../../src/link.html"); ?>
 
     <div class="container">
 
@@ -141,6 +132,7 @@
             Zkuste si zatím na <a href="https://blockly.games/?lang=cs" target="_blank">blockly.games</a>
             dát další levely v dalších kategoriích, dokud ostatní nedokončí svoji práci.
         </p>
+
     </div>
 
 </body>

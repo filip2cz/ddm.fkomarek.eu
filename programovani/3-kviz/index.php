5562798288283bc9cb1400d8c36e3cdf72883bc5
filip2cz <filip@fkomarek.eu>
2025-11-23 20:00:54 +0100

    ADD: kvíz

--- programovani/3-kviz/index.php
+++ programovani/3-kviz/index.php
@@ -23,7 +23,38 @@
         </p>
 
         <p>
-            Kód nezapomeňte uložit na <a href="https://classroom.google.com/c/NzE5NDk1NjM2NDAw/a/NzMwMjg2Mzg3MTc4/details" target="_blank">Google Učebnu</a>.
+            Příklad, jak může program vypadat:
+        </p>
+
+        <pre class="output">Jaká je nejblížší planeta ke slunci?
+a) Merkur
+b) Venuše
+c) Země
+d) Mars
+A
+Správně!
+
+Jak se jmenuje syn Homera Simpsona ze seriálu Simpsonovi?
+Bert
+Špatně! Správná odpověď je Bart.
+
+Kolik má planeta Země kontinentů?
+a) 8
+b) 4
+c) 6
+d) 2
+B
+Špatně! Správná odpověď je C.
+
+Kolik minut má jedna hodina?
+60
+Správně!
+
+Vaše známka: 3 (správně máte 2 ze 4 otázek)
+        </pre>
+
+        <p>
+            Kód nezapomeňte uložit na <a href="https://classroom.google.com/c/ODA4MjE5ODMxODM0/a/ODMwNzU2NDY0NzQz/details" target="_blank">Google Učebnu</a>.
         </p>
 
     </div>
